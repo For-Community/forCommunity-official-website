eadcca3d233f16bd43cc3298b68e1b5d9739a72c
Justin Varghese <69856008+Justinnn07@users.noreply.github.com>
2020-12-03 21:54:12 +0530

    added some css (#3)

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="body">
+      <img
+        className="image-body"
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Aug/1500x600_Hero-Tall_np._CB404803728_.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Body;
