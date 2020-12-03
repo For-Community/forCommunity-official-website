5d64fe5e6c2bc8d28a38f98cc8c690af22977cd3
Justinnn07 <justinv2006@gmail.com>
2020-12-03 21:37:38 +0530

    added some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header />
+      {/* Body */}
+      <Body />
     </div>
   );
 }
