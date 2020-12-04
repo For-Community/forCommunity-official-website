50d685561dca96c36c9ac91dbac076f42427bece
stephin007 <stephinreji123@gmail.com>
2020-12-04 08:23:55 +0530

    Added body section component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,12 @@
-import React from "react";
+import React, {Fragment} from 'react'
+import BodySection from "./BodySection"
 
 function Body() {
   return (
-    <div className="body">
-      <img
-        className="image-body"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Aug/1500x600_Hero-Tall_np._CB404803728_.jpg"
-        alt=""
-      />
-    </div>
-  );
+    <Fragment>
+      <BodySection/>
+    </Fragment>
+  )
 }
 
-export default Body;
+export default Body
---
+++ src/components/BodySection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BodySection = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BodySection
