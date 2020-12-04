c82025ed0c01f24b7c5bfe16b3fd0bf4cf9bdb39
stephin007 <stephinreji123@gmail.com>
2020-12-04 09:46:03 +0530

    fixed responsiveness:hammer:

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ const HeroSection = () => {
     return (
         <div className="hero">
             <div class="hero-flex hero-text">
-                <p>To find everything in something</p>
+                <h4>To find <span>everything</span> in <span>something...</span></h4>
             </div>
             <div class="hero-flex hero-image">
                 <img src={HeroImg} alt="hero-img" height={"350px"} />
