973aac3a2879ba297646f50a75b3543b16894d5a
Justinnn07 <justinv2006@gmail.com>
2020-12-05 09:39:57 +0530

    added about sec

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="about-text">
+        <h3>About us</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          corrupti fugiat, nesciunt accusamus nulla repellendus neque deserunt
+          temporibus unde harum odio minima quasi quidem, minus facere nostrum
+          sint molestiae cum!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
