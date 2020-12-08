be1e1ee6aeef782f4dafeb22435ef2aa1d2f72b3
Justinnn07 <justinv2006@gmail.com>
2020-12-08 12:53:00 +0530

    added git + site sec in projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 
 import "./App.css";
 import Header from "./components/Header";
@@ -7,17 +7,19 @@ import Home from "./components/Home";
 import TeamSection from "./components/TeamSection";
 import Connect from "./components/Connect";
 import Footer from "./components/Footer";
+import Project from "./components/Project";
 
 function App() {
   return (
-      <Fragment>
-        <Header />
-        <Home />
-        <About />
-        <TeamSection />
-        <Connect />
-        <Footer />
-      </Fragment>
+    <Fragment>
+      <Header />
+      <Home />
+      <About />
+      <TeamSection />
+      <Project />
+      <Connect />
+      <Footer />
+    </Fragment>
   );
 }
 
