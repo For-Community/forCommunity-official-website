c41a7494d1ad751f3284f2ae7fc027e5578d1318
stephin007 <stephinreji123@gmail.com>
2020-12-11 19:31:11 +0530

    Made the team section fully dynamic:sparkles:

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -1,87 +1,41 @@
-import React from 'react'
-
-import team1 from "../assets/team1.jpg"
-import team2 from "../assets/team2.jpg"
-import team3 from "../assets/team3.jpg"
-import team4 from "../assets/team4.jpg"
+import React, {useState, useEffect} from 'react'
+import TeamSectionData from "../data/TeamSectionData"
+import TeamCard from "./TeamCard"
 
 function TeamSection() {
+    const [teamData, setTeamData] = useState([])
+
+    useEffect(() => {
+        setTeamData(TeamSectionData);
+        console.log(TeamSectionData)
+    },[])
+
     return (
-        <div className="team-section" id="community">
-            <div class="team-container">
-                <div class="team-row">
-                    <div class="team-title">
-                        <h1>Our Community</h1>
-                        <p>A small yet growing team of members</p>
-                    </div>
-                </div>
-                <div class="team-row">
-                    <div class="team-items">
-                        <div class="team-item">
-                            <img src={team1} alt="profile img" />
-                            <div class="inner">
-                                <div class="info">
-                                    <h5>Name 1</h5>
-                                    <p>Founder, React Dev</p>
-                                    <div class="social-links">
-                                        <a href="/"><span className="fa fa-github"></span></a>
-                                        <a href="/"><span className="fa fa-twitter"></span></a>
-                                        <a href="/"><span className="fa fa-linkedin"></span></a>
-                                        <a href="/"><span className="fa fa-facebook"></span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="team-item">
-                            <img src={team2} alt="profile img" />
-                            <div class="inner">
-                                <div class="info">
-                                    <h5>Name 2</h5>
-                                    <p>Designer</p>
-                                    <div class="social-links">
-                                        <a href="/"><span className="fa fa-github"></span></a>
-                                        <a href="/"><span className="fa fa-twitter"></span></a>
-                                        <a href="/"><span className="fa fa-linkedin"></span></a>
-                                        <a href="/"><span className="fa fa-facebook"></span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="team-item">
-                            <img src={team3} alt="profile img" />
-                            <div class="inner">
-                                <div class="info">
-                                    <h5>Name 3</h5>
-                                    <p>UI/UX</p>
-                                    <div class="social-links">
-                                        <a href="/"><span className="fa fa-github"></span></a>
-                                        <a href="/"><span className="fa fa-twitter"></span></a>
-                                        <a href="/"><span className="fa fa-linkedin"></span></a>
-                                        <a href="/"><span className="fa fa-facebook"></span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="team-item">
-                            <img src={team4} alt="profile img" />
-                            <div class="inner">
-                                <div class="info">
-                                    <h5>Name 4</h5>
-                                    <p>Machine Learning</p>
-                                    <div class="social-links">
-                                        <a href="/"><span className="fa fa-github"></span></a>
-                                        <a href="/"><span className="fa fa-twitter"></span></a>
-                                        <a href="/"><span className="fa fa-linkedin"></span></a>
-                                        <a href="/"><span className="fa fa-facebook"></span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <div className="team-section" id="community">
+        <div class="team-container">
+          <div class="team-row">
+            <div class="team-title">
+              <h1>Our Community</h1>
+              <p>A small yet growing team of members</p>
             </div>
+          </div>
+          <div class="team-row">
+            <div class="team-items">
+              {
+                teamData.map((team) => (
+                  <TeamCard
+                        key={team.id}
+                    img={team.img}
+                    name={team.name}
+                    designation={team.designation}
+                    sociallinks={team.sociallinks}
+                  />
+                ))}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default TeamSection
