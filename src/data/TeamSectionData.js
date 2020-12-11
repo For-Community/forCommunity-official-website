e6bbdec623630accef479a36aa08da279bde4a30
stephin007 <stephinreji123@gmail.com>
2020-12-11 21:08:45 +0530

    edited teamSectiondata

--- src/data/TeamSectionData.js
+++ src/data/TeamSectionData.js
@@ -1,7 +1,5 @@
 import team1 from "../assets/team1.jpg";
 import team2 from "../assets/team2.jpg";
-import team3 from "../assets/team3.jpg";
-import team4 from "../assets/team4.jpg";
 
 const TeamSectionData = [
   {
