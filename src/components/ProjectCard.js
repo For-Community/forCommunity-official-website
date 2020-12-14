3c4ebd23327cbd7d569430cfeac49c1a7e66a10f
Stephin Reji <stephinreji123@gmail.com>
2020-12-14 23:53:47 +0530

    Dynamic project data (#12)
    * Added project section json
    
    * Changed the projects and project card component according to the json:octocat:

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-function ProjectCard({ img, title, git, site }) {
+function ProjectCard({ img, desc, links}) {
   return (
     <div className="projectCard">
       <img src={img} alt="" className="image" style={{ width: "100%" }} />
       <div className="project-middle">
         <div className="project-text">
-          {title}
+          {desc}
           <div className="btn">
-            <a href={git}>
+            <a href={links.git}>
               <button className="button">
                 <i class="fab fa-github"></i>
               </button>
             </a>
-            <a href={site}>
+            <a href={links.web}>
               <button className="button">
                 <i class="fab fa-firefox-browser"></i>
               </button>
