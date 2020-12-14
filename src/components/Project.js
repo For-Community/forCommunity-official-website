0c98d2a30a1abd01d8275ffb679075b15fe5b90a
stephin007 <stephinreji123@gmail.com>
2020-12-14 23:19:42 +0530

    Changed the projects and project card component according to the json:octocat:

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import ProjectSectionData from "../data/ProjectSectionData"
 import ProjectCard from "./ProjectCard";
 
 function Project() {
+  const [projectData, setProjectData] = useState([])
+
+  useEffect(() => {
+    setProjectData(ProjectSectionData);
+    console.log(ProjectSectionData);
+  }, []);
+
   return (
     <div className="project" id="project">
       <h2>Check out some of our builds</h2>
       <div className="pro-card">
-        <ProjectCard
-          img="https://justinnn07.netlify.app/images/portfolio/covid.PNG"
-          title="hahah"
-          git="https://github.com/for-Community"
-          site="https://forcommunityy.web.app/"
-        />
-        <ProjectCard
-          img="https://justinnn07.netlify.app/images/portfolio/discord.PNG"
-          title="hahah"
-          git="https://github.com/for-Community"
-          site="https://forcommunityy.web.app/"
-        />
+        {
+                projectData.map((project) => (
+                  <ProjectCard
+                    key={project.id}
+                    img={project.imglink}
+                    desc={project.desc}
+                    designation={project.designation}
+                    links={project.links}
+                  />
+                ))}
       </div>
     </div>
   );
