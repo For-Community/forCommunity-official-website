5765b8124bda6d91fc0f94c2ff254b1868c9e6fc
stephin007 <stephinreji123@gmail.com>
2020-12-14 23:18:03 +0530

    Added project section json

---
+++ src/data/ProjectSectionData.js
@@ -0,0 +1,24 @@
+const ProjectSectionData = [
+  {
+    id: 1,
+    imglink: "https://justinnn07.netlify.app/images/portfolio/covid.PNG",
+    desc: "Real Time Covid-19 Tracker",
+    techstack: "React JS",
+    links: {
+      web: "https://covid-tracker-989.web.app",
+      git: "https://github.com/Justinnn07/Covid-Tracker",
+    },
+  },
+  {
+    id: 2,
+    imglink: "https://justinnn07.netlify.app/images/portfolio/discord.PNG",
+    desc: "Clone of Discord",
+    techstack: "React JS",
+    links: {
+      web: "https://discord-clone-989.web.app",
+      git: "https://github.com/For-Community/discord-clone",
+    },
+  },
+];
+
+export default ProjectSectionData;
