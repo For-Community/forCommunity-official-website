85e10883b9f24f469738180bfd941b0216af580e
stephin007 <stephinreji123@gmail.com>
2020-12-22 18:53:24 +0530

    DYnamic Connect section with added extra connect option

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,34 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import ConnectSectionData from "../data/ConnectSectionData";
 
 const Contact = () => {
+    const [connectData, setConnectData] = useState([]);
+
+    useEffect(() => {
+      setConnectData(ConnectSectionData);
+      console.log(ConnectSectionData);
+    }, []);
+
+
     return (
-        <div className="contact-section" id="connect">
-            <div class="contact-container">
-                <div class="contact-row">
-                    <div class="contact-title">
-                        <h1>Want to Join Us?</h1>
-                        <p>Our Social Media Presence</p>
-                    </div>
-                </div>
-                <div class="social-row">
-                    <div class="social-info">
-                        <div class="social-card">
-                            <a href="mailto:mail@stephinreji.me"><i class="social-card-icon fa fa-envelope" /></a>
-                            <p>Email</p>
-                        </div>
-                        <div class="social-card">
-                            <a href="https://github.com/For-Community/"><i class="social-card-icon fa fa-github" /></a>
-                            <p>Github</p>
-                        </div>
-                        <div class="social-card">
-                            <a href="https://discord.gg/zRDZG3uM53"><i class="social-card-icon fab fa-discord" /></a>
-                            <p>Discord</p>
-                        </div>
-                    </div>
-                </div>
+      <div className="contact-section" id="connect">
+        <div class="contact-container">
+          <div class="contact-row">
+            <div class="contact-title">
+              <h1>Want to Join Us?</h1>
+              <p>Our Social Media Presence</p>
             </div>
+          </div>
+          <div class="social-row">
+            <div class="social-info">
+              {connectData.map((connect) => (
+                <div class="social-card">
+                  <a href={connect.sociallink}>
+                    <i class={`social-card-icon ${connect.faclass}`} />
+                  </a>
+                      <p>{connect.sociallinktext}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Contact
