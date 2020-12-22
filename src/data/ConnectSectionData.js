fb496189979bbbc70f33dd1ca45c5f40890236c8
Stephin Reji <stephinreji123@gmail.com>
2020-12-22 18:54:45 +0530

    DYnamic Connect section with added extra connect option (#15)

---
+++ src/data/ConnectSectionData.js
@@ -0,0 +1,28 @@
+const ConnectSectionData = [
+  {
+    id: 1,
+    sociallink: "mailto:mail@stephinreji.me",
+    faclass: "fa fa-envelope",
+    sociallinktext: "Email",
+  },
+  {
+    id: 2,
+    sociallink: "https://github.com/For-Community/",
+    faclass: "fa fa-github",
+    sociallinktext: "Github",
+  },
+  {
+    id: 3,
+    sociallink: "https://discord.gg/zRDZG3uM53",
+    faclass: "fab fa-discord",
+    sociallinktext: "Discord",
+  },
+  {
+    id: 4,
+    sociallink: "https://dev.to/forcommunity",
+    faclass: "fab fa-dev",
+    sociallinktext: "Dev",
+  },
+];
+
+export default ConnectSectionData;
