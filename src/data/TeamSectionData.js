0f013550f262d06d4e6aae1dbb9b4a9eb8f8750b
root <root@localhost.localdomain>
2020-12-25 00:05:26 +0530

    committed

--- src/data/TeamSectionData.js
+++ src/data/TeamSectionData.js
@@ -34,11 +34,35 @@ const TeamSectionData = [
     img: team3,
     sociallinks: {
       github: "https://github.com/masspher",
-      twitter:"https://twitter.com/abhijiths_2003?s=20",
+      twitter: "https://twitter.com/abhijiths_2003?s=20",
       linkedin: "https://www.linkedin.com/in/abhijith-s-bb8a24201",
       facebook: "https://www.facebook.com/profile.php?id=100049296183097",
     },
   },
+  {
+    id: "4",
+    name: "Akshay V Doizode",
+    designation: "ML Specialist",
+    img: team3,
+    sociallinks: {
+      github: "https://github.com/akshayvdoizode",
+      twitter: "/",
+      linkedin: "https://www.linkedin.com/in/akshay-v-doizode-074895b4/",
+      facebook: "/",
+    },
+  },
+  {
+    id: "5",
+    name: "Vansh Sehgal",
+    designation: "MERN Stack",
+    img: team3,
+    sociallinks: {
+      github: "https://github.com/vanshsehgal2001/",
+      twitter: "https://twitter.com/Vansh_Sehgal_18",
+      linkedin: "https://www.linkedin.com/in/vansh-sehgal-1519391a8/",
+      facebook: "https://www.facebook.com/vansh.sehgal.790",
+    },
+  },
 ];
 
-export default TeamSectionData
+export default TeamSectionData;
