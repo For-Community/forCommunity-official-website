5dafb191bc6f19de017a1fb2491d67d10f2bd458
Justinnn07 <justinv2006@gmail.com>
2020-12-24 16:20:19 +0530

    added btn to projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-
 import ProjectSectionData from "../data/ProjectSectionData";
 import ProjectCard from "./ProjectCard";
 
 function Project() {
   const [projectData, setProjectData] = useState([]);
+  const [visiblity, setVisiblity] = useState(3);
 
+  const viewmore = () => {
+    setVisiblity(visiblity + 2);
+  };
   useEffect(() => {
     setProjectData(ProjectSectionData);
     // console.log(ProjectSectionData);
@@ -19,21 +22,29 @@ function Project() {
             <h1>Check out some of our builds</h1>
           </div>
         </div>
-
         <div class="project-row">
           <div class="project-items">
-              {projectData.map((project) => (
-                <ProjectCard
-                  key={project.id}
-                  img={project.imglink}
-                  title={project.title}
-                  desc={project.desc}
-                  designation={project.designation}
-                  links={project.links}
-                />
-              ))}
+            {projectData.slice(0, visiblity).map((project) => (
+              <ProjectCard
+                key={project.id}
+                img={project.imglink}
+                title={project.title}
+                desc={project.desc}
+                designation={project.designation}
+                links={project.links}
+              />
+            ))}
           </div>
         </div>
+      </div>
+      <div className="btn-h">
+        {visiblity < 5 ? (
+          <button className="btn-btn1" onClick={viewmore}>
+            Load more{" "}
+          </button>
+        ) : (
+          <button style={{ display: "none" }}>LOad more</button>
+        )}
       </div>
     </div>
   );
