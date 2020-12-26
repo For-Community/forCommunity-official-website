7135bc0ae0c57b33ca9d9ffac5a483e136e46eb7
stephin007 <stephinreji123@gmail.com>
2020-12-26 09:21:33 +0530

    Added Live website visitor counter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import HeroImg from "../assets/home.png";
 
 const HeroSection = () => {
+  const [count, setCount] = useState()
+
+  useEffect(() => {
+    async function fetchCount() {
+      let response = await fetch(
+        "https://api.countapi.xyz/update/fcommunity.com/fcommunity?amount=1"
+      );
+      response = await response.json();
+      setCount(response.value)
+    }
+    fetchCount();
+  },[])
+
   return (
     <div className="hero" id="home">
+      <div class="counter">
+        <h4>Visitors: {count }</h4>
+      </div>
       <div class="hero-flex hero-text">
         <h4>
           To find <span>everything</span> in <span>something...</span>
