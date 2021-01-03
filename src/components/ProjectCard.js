02ff3f16666d277b73393442befa5d90095cef41
root <root@localhost.localdomain>
2021-01-03 21:47:02 +0530

    committed

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,48 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
 
-function ProjectCard({ title, img, desc, git, web }) {
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== "production") {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
+function ProjectCard({ title, img, desc, web, git }) {
+  const [langInfo, setLangInfo] = useState({});
+
+  useEffect(() => {
+    fetchLangs();
+  }, []);
+
+  const fetchLangs = async () => {
+    if (git !== "") {
+      let l = git;
+      let arr = l.split("/");
+      let repoName = arr[arr.length - 1];
+      let userName = arr[arr.length - 2];
+      const response = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://api.github.com/repos/${userName}/${repoName}/languages?client_id=${githubClientId}&client_secret=${githubClientSecret}`
+      );
+      const data = response.data;
+      // console.log(data);
+      let sum = 0;
+      for (const prop in data) {
+        sum = sum + data[prop];
+      }
+      for (const prop in data) {
+        data[prop] = parseInt((data[prop] / sum) * 100);
+      }
+      console.log(data);
+      // console.log(sum);
+      setLangInfo(data);
+    }
+  };
+
   return (
     <Fragment>
       <div className="project-item">
@@ -9,6 +51,38 @@ function ProjectCard({ title, img, desc, git, web }) {
           <div class="info">
             <h2>{title}</h2>
             <p>{desc}</p>
+            <div className="languages">
+              <h3
+                style={{
+                  textAlign: "center",
+                  marginTop: "30px",
+                  fontSize: "20px",
+                }}
+              >
+                Languages
+              </h3>
+              {langInfo &&
+                Object.keys(langInfo).map((key) => {
+                  return (
+                    <Fragment>
+                      <li>
+                        <h3 style={{ color: "darkcyan", fontWeight: "bolder" }}>
+                          {" "}
+                          {key} ({langInfo[key]}%)
+                        </h3>
+                        <span className="bar">
+                          <span
+                            style={{
+                              width: `${langInfo[key]}%`,
+                              animation: "ease-in-out 2s",
+                            }}
+                          ></span>
+                        </span>
+                      </li>
+                    </Fragment>
+                  );
+                })}
+            </div>
             <div className="social-links">
               <a
                 href={git}
