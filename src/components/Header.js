cbcf0e43ba1d236892762a996cd05a490c4c03af
stephin007 <stephinreji123@gmail.com>
2021-01-23 10:23:46 +0530

    Removed the visitors count

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,28 @@ import React from "react";
 
 import logo from "../assets/logo.png";
 
-const Header = ({setDark, dark}) => {
+const Header = ({ setDark, dark }) => {
   return (
     <div className="header">
+      <div className="wrapDark">
+        <div className="header-logo">
+          <img src={logo} alt="For Community Logo" />
+          <h3>
+            For<span>Community</span>
+          </h3>
+        </div>
+        <div className="header-link">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#community">Community</a>
+          <a href="#project">Projects</a>
+          <a href="#connect">Connect</a>
+        </div>
 
-    
-     <div className="wrapDark">
-       
-     <div className="header-logo">
-        <img src={logo} alt="For Community Logo" />
-        <h3>
-          For<span>Community</span>
-        </h3>
+        <button className="ColorButton" onClick={() => setDark((dark = !dark))}>
+          {dark ? <i class="fas fa-sun"></i> : <i class="fas fa-moon"></i>}
+        </button>
       </div>
-      <div className="header-link">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#community">Community</a>
-        <a href="#project">Projects</a>
-        <a href="#connect">Connect</a>
-      
-     
-      </div>
-
-      <button className = 'ColorButton' onClick = {() => setDark(dark = !dark)}>{dark ? <i class="fas fa-sun"></i>:<i class="fas fa-moon"></i>}</button>
- 
-     
-       </div> 
-     
-
- 
-   
-   
     </div>
   );
 };
